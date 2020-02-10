73958352644b680194c297bbc3d1f28f2b6822e0
Marvin Reyes <reyemarv@gmail.com>
2020-02-10 01:04:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,9 @@ class School
   
   def add_student(name, grade)
       if roster.include? grade
-        roster[:grade] << name 
+        roster[grade] << name 
+      else 
+        
   end
   
 end 
