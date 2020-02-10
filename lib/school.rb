cd3bac087a9ad3f1417623da64c9a41a8b90eaa7
Marvin Reyes <reyemarv@gmail.com>
2020-02-10 01:12:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,7 +14,7 @@ class School
       if roster.include? grade
         roster[grade] << name 
       else 
-        roster = [grade]<< name.collect {|name| name}
+        roster = [grade]<< name.map{|name| name}
       end 
   end
   
